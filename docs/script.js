bc5e4d1ec72a16757d90d09f0a0682efaf45a0c8
Nazar4119 <nazarvdovicenko971@gmail.com>
2025-03-02 13:24:44 +0100

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript файл успішно підключений і готовий до роботи.');
+
+    const colors = [
+        '#590d22', '#800f2f', '#a4133c', '#c9184a', '#ff4d6d',
+        '#ff758f', '#ff8fa3', '#ffb3c1', '#ffccd5', '#fff0f3'
+    ];
+    const mosaicContainer = document.createElement('div');
+    mosaicContainer.classList.add('mosaic');
+
+    const numCells = Math.ceil(window.innerWidth / 55) * Math.ceil(window.innerHeight / 55);
+
+    for (let i = 0; i < numCells; i++) {
+        const cell = document.createElement('div');
+        cell.style.backgroundColor = colors[i % colors.length];
+        mosaicContainer.appendChild(cell);
+    }
+
+    document.body.appendChild(mosaicContainer);
+});
